4308c2134edc5f28bf57c2e379e3a83ec5743206
leoboyerbx <leo.boyer@outlook.com>
2018-09-13 11:25:22 +0200

    Pointed CSS adresses to CDN

--- assets/js/modules/cssrules.js
+++ assets/js/modules/cssrules.js
@@ -4,22 +4,25 @@ let importCss = function(target, media) {
     let link = document.createElement('link')
     link.rel = "stylesheet"
     link.href = target
-    link.type = 'text/css'
+    
     if (media) link.media = media
     return link
 }
 class CssRules {
     static init () {
+        let faCSS = 'https://cdn.rawgit.com/leoboyerbx/ColorJS/cdn/assets/fontawesome/css/fontawesome-all.css'
+        let globalCSS = "https://cdn.rawgit.com/leoboyerbx/ColorJS/cdn/assets/css/colorjs.css"
+        let printCSS = "https://cdn.rawgit.com/leoboyerbx/ColorJS/cdn/assets/css/print.css"
         let existingStyleSheet = document.querySelector('link') || document.querySelector('style')
         if (existingStyleSheet) {
-            qS('head').insertBefore(importCss('assets/css/colorjs.css'), existingStyleSheet)
-            qS('head').insertBefore(importCss('assets/fontawesome/css/fontawesome-all.css'), existingStyleSheet)
-            qS('head').insertBefore(importCss('assets/css/print.css', 'print'), existingStyleSheet)
+            qS('head').insertBefore(importCss(globalCSS), existingStyleSheet)
+            qS('head').insertBefore(importCss(faCSS), existingStyleSheet)
+            qS('head').insertBefore(importCss(printCSS, 'print'), existingStyleSheet)
 
         } else {
-            qS('head').appendChild(importCss('assets/css/colorjs.css'))
-            qS('head').appendChild(importCss('assets/fontawesome/css/fontawesome-all.css'))
-            qS('head').appendChild(importCss('assets/css/print.css', 'print'))
+            qS('head').appendChild(importCss(globalCSS))
+            qS('head').appendChild(importCss(faCSS))
+            qS('head').appendChild(importCss(printCSS, 'print'))
         }
     }
 }
